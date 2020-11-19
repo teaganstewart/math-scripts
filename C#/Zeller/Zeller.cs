315ec85fdb4fa797086ce74245edc72044248a14
teagachu <teagachu@users.noreply.github.com>
2020-11-20 11:14:52 +1300

    Fixed Day entered never valid issue
    Fixed the error in the check number method, that required a number to be larger then 1000 which was breaking the day input. Instead a number now has to be larger then 1000 only if it is a year input.

--- C#/Zeller/Zeller.cs
+++ C#/Zeller/Zeller.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 
-namespace App
-{
-    class ZellerBirthday
-    {
+namespace App {
+    
+    class ZellerBirthday {
+
+        int dayNumber;
+        int monthNumber;
+        int yearNumber;
+
         int[] days = new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         string[] months = new string[] {"january", "february", "march", "april", "may", "june", 
                                         "july", "august", "september", "october", "november", "december"};
@@ -15,14 +19,14 @@ namespace App
             Console.Write("Enter your year of birth- ");
             string year = Console.ReadLine();
 
-            while(!CheckNumber(year)) {
+            while(!CheckNumber(year, true)) {
 
-                Console.WriteLine("Not a valid year. Must be 0 < year < 2020.");
+                Console.WriteLine("Not a valid year. Must be 1000 <= year <= 2020.");
                 Console.Write("Enter your year of birth- ");
                 year = Console.ReadLine();
             }
 
-            int yearNumber = Int32.Parse(year);
+            yearNumber = Int32.Parse(year);
 
             // Second check the month as it affects the valid days.
             Console.Write("Enter your month of birth- ");
@@ -46,17 +50,27 @@ namespace App
                 day = Console.ReadLine();
             }    
 
-            int dayNumber = Int32.Parse(day);
+            dayNumber = Int32.Parse(day);
+            monthNumber = Array.IndexOf(months, month) + 1;
+            if(monthNumber <= 2) { 
+                monthNumber += 12; 
+                yearNumber -= 1;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Date Chosen: " + day + " " + month + " " + year);
         }
 
         // Checks whether a number is valid.
         /// <param value="str"> The string value of the number. </param>
-        bool CheckNumber(string str) {
+        bool CheckNumber(string str, bool year) {
+
             int value;
             if(int.TryParse(str, out value)) {
-                if(value > 0 && value <= 2020) {
+                if(year && value >= 1000 && value <= 2020) {
                     return true;
                 }
+                if(!year) { return true; }
             }
 
             return false;
@@ -64,7 +78,8 @@ namespace App
 
         // Checks whether day is valid based on the month.
         bool CheckDayInput(string day, string month, int year) {
-            if(CheckNumber(day)) {
+
+            if(CheckNumber(day, false)) {
                 int dayNumber = Int32.Parse(day);
                 int index = Array.IndexOf(months, month);
                 if(index == 1) {
@@ -82,6 +97,7 @@ namespace App
 
         // Checks the given year, so I know whether there are 29 or 28 days in February.
         bool checkLeapYear(int year) {
+
             if(year % 4 == 0) {
                 if(year % 100 == 0) {
                     if(year % 400 == 0) {
@@ -98,16 +114,19 @@ namespace App
 
         // Checks if the month entered is valid and in the list.
         bool CheckMonthInput(string month) {
-            return (Array.IndexOf(months, month) > -1);
 
+            return (Array.IndexOf(months, month) > -1);
         }
 
         void Zeller() {
+
             GetInput();
+            int yf = Int32.Parse(dayNumber.ToString().Substring(0,2));
+            Console.WriteLine(yf);
+
         }
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             new ZellerBirthday().Zeller();
         }
     }
