44666e285fd0452698505b03421ac22385dbf77b
teagachu <teagachu@users.noreply.github.com>
2020-11-20 13:15:37 +1300

    Updated to Add Tests

---
+++ C#/GCD/GcdTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GcdTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("hi", "hi");
+        }
+    }
+}
---
+++ C#/Zeller/ZellerTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ZellerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("hi", "bye");
+        }
+    }
+}
